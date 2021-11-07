e7c1495f75a0079add29e6df408f811b29d423c7
juan-uribe <89612252+juan-uribe@users.noreply.github.com>
2021-11-07 15:22:27 -0600

    Actualizacion bd

--- PHPDocumentos/prueba_login.sql
+++ PHPDocumentos/prueba_login.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-11-2021 a las 04:30:40
--- Versión del servidor: 10.1.37-MariaDB
--- Versión de PHP: 7.3.1
+-- Tiempo de generación: 07-11-2021 a las 22:21:58
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -49,7 +48,39 @@ INSERT INTO `archivos` (`id`, `titulo`, `tipo`, `tamanio`, `ruta`, `fecha`, `con
 (2, 'ReporteDeMuestras', 'pdf', '62', 'uploads/ReporteDeMuestras.pdf', '2021-11-12', ''),
 (1, 'Recetas', 'txt', '2', 'uploads/Recetas.txt', '2021-11-02', ''),
 (21, 'respuestaensayo', 'txt', '0', 'uploads/respuestaensayo.txt', '2021-11-05', 'Prueba'),
-(22, 'logo_adaptstorage_negro', 'png', '47798', 'uploads/logo_adaptstorage_negro.png', '2021-11-06', 'Prueba');
+(22, 'logo_adaptstorage_negro', 'png', '47798', 'uploads/logo_adaptstorage_negro.png', '2021-11-06', 'Prueba'),
+(23, 'the-game-plan', 'txt', '3145', 'uploads/the-game-plan.txt', '2021-11-06', 'Prueba'),
+(24, 'qwertyadj', 'mp3', '1920939', 'uploads/qwertyadj.mp3', '2021-11-06', 'Prueba'),
+(25, 'qwertyadj', 'mp3', '1920939', 'uploads/qwertyadj.mp3', '2021-11-06', 'Prueba'),
+(26, 'autorizando-ableton-live-offline', 'txt', '524', 'uploads/autorizando-ableton-live-offline.txt', '2021-11-06', 'Prueba'),
+(27, 'guio??n-te??cnico_sonido_16jun21', 'xlsx', '112270', 'uploads/guio??n-te??cnico_sonido_16jun21.xlsx', '2021-11-06', 'Prueba'),
+(28, 'wipefile', '7z', '127965', 'uploads/wipefile.7z', '2021-11-06', 'Prueba'),
+(29, 'wipefile', '7z', '127965', 'uploads/wipefile.7z', '2021-11-06', 'Prueba'),
+(30, 'wipefile', '7z', '127965', 'uploads/wipefile.7z', '2021-11-06', 'Prueba'),
+(31, 'wipefile', '7z', '127965', 'uploads/wipefile.7z', '2021-11-06', 'Prueba'),
+(32, 'wipefile', '7z', '127965', 'uploads/wipefile.7z', '2021-11-06', 'Prueba'),
+(33, 'wipefile', '7z', '127965', 'uploads/wipefile.7z', '2021-11-06', 'Prueba'),
+(34, 'wipefile', '7z', '127965', 'uploads/wipefile.7z', '2021-11-06', 'Prueba'),
+(35, 'horario', 'jpg', '443193', 'uploads/horario.jpg', '2021-11-06', 'Prueba'),
+(36, 'horario', 'jpg', '443193', 'uploads/horario.jpg', '2021-11-06', 'Prueba'),
+(37, 'c.-tangana---demasiadas-mujeres', 'mp3', '4026906', 'uploads/c.-tangana---demasiadas-mujeres.mp3', '2021-11-06', 'Prueba'),
+(38, 'c.-tangana---demasiadas-mujeres', 'mp3', '4026906', 'uploads/c.-tangana---demasiadas-mujeres.mp3', '2021-11-06', 'Prueba'),
+(39, 'c.-tangana---demasiadas-mujeres', 'mp3', '4026906', 'uploads/c.-tangana---demasiadas-mujeres.mp3', '2021-11-06', 'Prueba'),
+(40, 'c.-tangana---demasiadas-mujeres', 'mp3', '4026906', 'uploads/c.-tangana---demasiadas-mujeres.mp3', '2021-11-06', 'Prueba'),
+(41, 'c.-tangana---demasiadas-mujeres', 'mp3', '4026906', 'uploads/c.-tangana---demasiadas-mujeres.mp3', '2021-11-06', 'Prueba'),
+(42, 'c.-tangana---demasiadas-mujeres', 'mp3', '4026906', 'uploads/c.-tangana---demasiadas-mujeres.mp3', '2021-11-06', 'Prueba'),
+(43, 'c.-tangana---demasiadas-mujeres', 'mp3', '4026906', 'uploads/c.-tangana---demasiadas-mujeres.mp3', '2021-11-07', 'Prueba'),
+(44, 'ukulele-4---base-free-instrumental-ukulele-acustico-compor-improvisar-bpm-84', 'mp3', '3459443', 'uploads/ukulele-4---base-free-instrumental-ukulele-acustico-compor-improvisar-bpm-84.mp3', '2021-11-07', 'Prueba'),
+(45, 'ukulele-4---base-free-instrumental-ukulele-acustico-compor-improvisar-bpm-84', 'mp3', '3459443', 'uploads/ukulele-4---base-free-instrumental-ukulele-acustico-compor-improvisar-bpm-84.mp3', '2021-11-07', 'Prueba'),
+(46, 'ukulele-4---base-free-instrumental-ukulele-acustico-compor-improvisar-bpm-84', 'mp3', '3459443', 'uploads/ukulele-4---base-free-instrumental-ukulele-acustico-compor-improvisar-bpm-84.mp3', '2021-11-07', 'Prueba'),
+(47, 'ukulele-4---base-free-instrumental-ukulele-acustico-compor-improvisar-bpm-84', 'mp3', '3459443', 'uploads/ukulele-4---base-free-instrumental-ukulele-acustico-compor-improvisar-bpm-84.mp3', '2021-11-07', 'Prueba'),
+(48, 'juggernaut-(audio)', 'mp3', '3531730', 'uploads/juggernaut-(audio).mp3', '2021-11-07', 'Prueba'),
+(49, 'autorizando-ableton-live-offline', 'txt', '524', 'uploads/autorizando-ableton-live-offline.txt', '2021-11-07', 'Prueba'),
+(50, 'autorizando-ableton-live-offline', 'txt', '524', 'uploads/autorizando-ableton-live-offline.txt', '2021-11-07', 'Prueba'),
+(51, 'autorizando-ableton-live-offline', 'txt', '524', 'uploads/autorizando-ableton-live-offline.txt', '2021-11-07', 'Prueba'),
+(52, 'qr-dra-parra', 'png', '3465', 'uploads/qr-dra-parra.png', '2021-11-07', 'Prueba'),
+(53, 'dra_icono', 'png', '566816', 'uploads/dra_icono.png', '2021-11-07', 'Prueba'),
+(54, 'autorizando-ableton-live-offline', 'txt', '524', 'uploads/autorizando-ableton-live-offline.txt', '2021-11-07', 'Prueba');
 
 -- --------------------------------------------------------
 
@@ -94,7 +125,9 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `usuario`, `clave`, `nombre`, `idTipoUsuario`) VALUES
 (1, 'dani', '$2y$10$r51CNOKhrQ9zp/ky6Ct2YeEv5kc6au4HBw4WTH87UgrP2EFsA5frG', 'Daniel Vazquez Franco', 1),
-(2, 'chats', '$2y$10$cuicSr3yZ0EMGRN7l57eqOEsJoWu39GH6b77aQTO8e/IwFdKioXcu', 'chats', 1);
+(2, 'chats', '$2y$10$cuicSr3yZ0EMGRN7l57eqOEsJoWu39GH6b77aQTO8e/IwFdKioXcu', 'chats', 4),
+(3, 'asd', '$2y$10$3g14mSWc.lGLLd7UJSiuourM2no23krYsHtjmotw4rSmUUJlRN41O', 'Juan Guillermo', 2),
+(4, '1', '$2y$10$zl60LEiB.5XrpFONRuU7OO2HTi6rPuDhSXxNH7Szv7TOzFza7mfJO', '1', 5);
 
 --
 -- Índices para tablas volcadas
@@ -126,7 +159,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `archivos`
 --
 ALTER TABLE `archivos`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo_usuario`
@@ -138,7 +171,7 @@ ALTER TABLE `tipo_usuario`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador único para el usuario', AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador único para el usuario', AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
